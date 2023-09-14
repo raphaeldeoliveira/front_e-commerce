dc460a9a65945c64dbc3065c2bea36a3087aa5d8
raphaeldeoliveira <rafaolive3003@gmail.com>
2023-09-14 14:46:13 -0300

    comecei a fazer o balao de carrinho. Ja consegui deixar sobreposto e estou lidando com o hover do mesmo

--- src/components/index/Header.js
+++ src/components/index/Header.js
@@ -2,7 +2,15 @@ import React from "react";
 
 export default function Header() {
 
+    const [showCarrinhoList, setShowCarrinhoList] = React.useState(false);
 
+    function mouseEntrouCarrinho() {
+        setShowCarrinhoList(true);
+    }
+
+    function mouseSaiuCarrinho() {
+        setShowCarrinhoList(false);
+    }
 
     return (
         <header>
@@ -10,14 +18,26 @@ export default function Header() {
 
             <div className="search-bar">
                 <input type="text" placeholder="O que você está procurando"/>
-                <div className="lupa-image">&#128269;</div>
+                <div className="lupa-image">
+                    <span>&#128269;</span>
+                </div>
             </div>
 
             <div>favoritos</div>
 
             <div>entrar</div>
 
-            <div>carrinho</div>
+            <div className="div-carrinho" onMouseLeave={mouseSaiuCarrinho}>
+                <div 
+                    onMouseEnter={mouseEntrouCarrinho}
+                    
+                    className="title"
+                >carrinho</div>
+                <div 
+                    className="balao-carrinho"
+                    style={{ display: showCarrinhoList ? "block" : "none" }}
+                ></div>
+            </div>
 
         </header>
     )
